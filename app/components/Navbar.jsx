f12d712e9f76ad9cf94479a1881a6fcf48fe2675
Luis Casas <luisscasas45@gmail.com>
2025-01-22 11:00:27 -0600

    FEAT/ link for navbar buttons

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-
+import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="bg-gradient-to-r from-[#B0005E] to-[#6C0036] py-4">
       <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
-        <img
-          src="/logos/logocultura.png"
-          alt="Logo Cultura"
-          className="h-16 mb-4"
-        />
+        <Link href="/dashboard">
+          <img
+            src="/logos/logocultura.png"
+            alt="Logo Cultura"
+            className="h-16 mb-4"
+          />
+        </Link>
         <div className="sm:flex space-x-6 text-white">
-          <button className="hover:text-gray-300">Registrar</button>
-          <button className="hover:text-gray-300">Historial</button>
-          <button className="hover:text-gray-300">Inventario</button>
+          <Link href="/formularioDeConsumible">
+            <button className="hover:text-gray-300">Registrar</button>
+          </Link>
+          <Link href="/historialDeRegistros">
+            <button className="hover:text-gray-300">Historial</button>
+          </Link>
+          <Link href="/inventario">
+            <button className="hover:text-gray-300">Inventario</button>
+          </Link>
         </div>
       </div>
     </div>
