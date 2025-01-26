727f6a2e687d31ee88daaac2e7db13b9f3365d10
Luis Casas <luisscasas45@gmail.com>
2025-01-26 15:47:28 -0600

    FEAT/ get the latest  5 records || delete analytics section

--- app/pages/dashboard.jsx
+++ app/pages/dashboard.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import Navbar from "@/components/Navbar";
 import ActivitySummary from "@/components/ActivitySummary";
 import InventoryStatus from "@/components/InventoryStatus";
-import Analytics from "@/components/Analytics";
 
 const Dashboard = () => {
   return (
     <div className="min-h-screen bg-[#F8F9FA]">
       <Navbar />
       <div className="p-6">
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 ">
           <ActivitySummary />
           <InventoryStatus />
-          <Analytics />
         </div>
       </div>
     </div>
