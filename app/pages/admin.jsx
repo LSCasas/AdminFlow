092cf461b4de74e7f40fa7d8eb5b1cc3dc6d851e
Luis Casas <luisscasas45@gmail.com>
2025-01-26 17:30:32 -0600

    FEAT/ add admin section to register new admins

--- app/pages/admin.jsx
+++ app/pages/admin.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
+import RegisterForm from "@/components/RegisterForm";
 
 const Admin = () => {
   return (
     <div className="min-h-screen bg-[#F8F9FA]">
       <Navbar />
+      <div className="flex justify-center items-center py-10">
+        <RegisterForm />
+      </div>
     </div>
   );
 };
--- app/pages/login.jsx
+++ app/pages/login.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "@/components/Navbar";
 import LoginForm from "@/components/LoginForm";
 
-const Admin = () => {
+export default function LoginPage() {
   return (
-    <div className="min-h-screen bg-[#F8F9FA] flex flex-col">
-      <Navbar />
+    <div className="bg-white min-h-screen flex flex-col">
       <div className="flex items-center justify-center flex-1">
         <div className="w-full max-w-md p-4">
           <LoginForm />
@@ -13,6 +11,4 @@ const Admin = () => {
       </div>
     </div>
   );
-};
-
-export default Admin;
+}
